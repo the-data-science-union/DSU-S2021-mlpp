1921fbe54f9ed6d0c0af38c47e1ce71e21b8eaf9
Victor Lin <vvictor.llin@gmail.com>
2021-01-11 21:08:20 -0800

    Include script aggregating dump .sql imports

---
+++ exploration/sql_migration/dump_import.sql
@@ -0,0 +1,29 @@
+create database IF NOT EXISTS osu_random_2020_08;
+USE osu_random_2020_08;
+SOURCE ~/Downloads/2020_08_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2020_08_01_performance_osu_random/osu_scores_high.sql;
+
+create database IF NOT EXISTS osu_random_2020_09;
+USE osu_random_2020_09;
+SOURCE ~/Downloads/2020_09_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2020_09_01_performance_osu_random/osu_scores_high.sql;
+
+create database IF NOT EXISTS osu_random_2020_10;
+USE osu_random_2020_10;
+SOURCE ~/Downloads/2020_10_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2020_10_01_performance_osu_random/osu_scores_high.sql;
+
+create database IF NOT EXISTS osu_random_2020_11;
+USE osu_random_2020_11;
+SOURCE ~/Downloads/2020_11_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2020_11_01_performance_osu_random/osu_scores_high.sql;
+
+create database IF NOT EXISTS osu_random_2020_12;
+USE osu_random_2020_12;
+SOURCE ~/Downloads/2020_12_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2020_12_01_performance_osu_random/osu_scores_high.sql;
+
+create database IF NOT EXISTS osu_random_2021_01;
+USE osu_random_2021_01;
+SOURCE ~/Downloads/2021_01_01_performance_osu_random/osu_user_stats.sql;
+SOURCE ~/Downloads/2021_01_01_performance_osu_random/osu_scores_high.sql;
