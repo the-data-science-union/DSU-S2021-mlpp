88cf54bdfaf06ac494b8842cfd79a14cf68081da
Victor Lin <vvictor.llin@gmail.com>
2021-01-27 09:58:12 -0800

    update dump_import to include difficulty attribs

--- exploration/sql_migration/dump_import.sql
+++ exploration/sql_migration/dump_import.sql
@@ -3,33 +3,51 @@ USE osu_random_2020_08;
 SOURCE 2020_08_01_performance_osu_random/osu_user_stats.sql;
 SOURCE 2020_08_01_performance_osu_random/osu_scores_high.sql;
 SOURCE 2020_08_01_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2020_08_01_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2020_08_01_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2020_08_01_performance_osu_random/osu_difficulty_attribs.sql;
 
 create database IF NOT EXISTS osu_random_2020_09;
 USE osu_random_2020_09;
 SOURCE 2020_09_01_performance_osu_random/osu_user_stats.sql;
 SOURCE 2020_09_01_performance_osu_random/osu_scores_high.sql;
 SOURCE 2020_09_01_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2020_09_01_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2020_09_01_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2020_09_01_performance_osu_random/osu_difficulty_attribs.sql;
 
 create database IF NOT EXISTS osu_random_2020_10;
 USE osu_random_2020_10;
 SOURCE 2020_10_03_performance_osu_random/osu_user_stats.sql;
 SOURCE 2020_10_03_performance_osu_random/osu_scores_high.sql;
 SOURCE 2020_10_03_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2020_10_03_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2020_10_03_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2020_10_03_performance_osu_random/osu_difficulty_attribs.sql;
 
 create database IF NOT EXISTS osu_random_2020_11;
 USE osu_random_2020_11;
 SOURCE 2020_11_01_performance_osu_random/osu_user_stats.sql;
 SOURCE 2020_11_01_performance_osu_random/osu_scores_high.sql;
 SOURCE 2020_11_01_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2020_11_01_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2020_11_01_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2020_11_01_performance_osu_random/osu_difficulty_attribs.sql;
 
 create database IF NOT EXISTS osu_random_2020_12;
 USE osu_random_2020_12;
 SOURCE 2020_12_01_performance_osu_random/osu_user_stats.sql;
 SOURCE 2020_12_01_performance_osu_random/osu_scores_high.sql;
 SOURCE 2020_12_01_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2020_12_01_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2020_12_01_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2020_12_01_performance_osu_random/osu_difficulty_attribs.sql;
 
 create database IF NOT EXISTS osu_random_2021_01;
 USE osu_random_2021_01;
 SOURCE 2021_01_01_performance_osu_random/osu_user_stats.sql;
 SOURCE 2021_01_01_performance_osu_random/osu_scores_high.sql;
 SOURCE 2021_01_01_performance_osu_random/osu_beatmaps.sql;
+SOURCE 2021_01_01_performance_osu_random/osu_beatmap_difficulty.sql;
+SOURCE 2021_01_01_performance_osu_random/osu_beatmap_difficulty_attribs.sql;
+SOURCE 2021_01_01_performance_osu_random/osu_difficulty_attribs.sql;
