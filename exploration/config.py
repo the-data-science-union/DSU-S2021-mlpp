88a6db5b43d06f0b41dfd7a7498498f10e38afc3
Victor Lin <vvictor.llin@gmail.com>
2021-02-17 02:31:34 -0800

    allow sql_inst to except db to use

--- exploration/config.py
+++ exploration/config.py
@@ -1,6 +1,9 @@
+from pymongo import MongoClient
+import pymysql.cursors
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+from pymysql.constants import CLIENT
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -10,13 +13,15 @@ SQL_PWD = os.getenv('SQL_PWD')
 SQL_HOST = os.getenv('SQL_HOST')
 MONGO_HOST = int(os.getenv('MONGO_HOST'))
 
-import pymysql.cursors
-from pymongo import MongoClient
 
-sql_inst = pymysql.connect(
-    host=SQL_HOST,
-    user=SQL_USER,
-    password=SQL_PWD
-)
+def sql_inst(db=None):
+    return pymysql.connect(
+        host=SQL_HOST,
+        user=SQL_USER,
+        password=SQL_PWD,
+        database=db,
+        client_flag=CLIENT.MULTI_STATEMENTS
+    )
+
 
 mongo_inst = MongoClient('localhost', MONGO_HOST)
