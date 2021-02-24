df65b1bafd4d2a2d7fa54c7542b56248ce35ea42
Sophie Yun <sophieyun1229@gmail.com>
2021-02-24 03:40:38 -0800

    refactor CDF and add attrib comparison

--- mlpp/data_modeling/sigmoid_fitting.py
+++ mlpp/data_modeling/sigmoid_fitting.py
@@ -1,4 +1,4 @@
-def fit_normal_cdf(bm_total, y):
+def fit_normal_cdf(y):
 
     """Fits a set of cartesian points to the normal cdf
 
@@ -10,33 +10,29 @@ def fit_normal_cdf(bm_total, y):
     Returns
     -------
     params (tuple): Tuple of parameters for the normal cdf
-    error (float): Least squares error from points given
+    error (float): Mean squared error from points given
     """
     import scipy
+    from scipy.stats import norm 
+    from scipy.optimize import curve_fit
     
-    def helper_take_out_zero(bm_total):
-        good_index = []
-        k = 0
-        for total in bm_total: 
-            if (total != 0):
-                good_index.append(k)
-            k = k + 1
-        bm_good_total = [bm_total[i] for i in good_index]
-        return bm_good_total
-
-    x = range(0, len(helper_take_out_zero(bm_total)))
+    n = len(y)
+    x = range(0, n)
+    try:
+        f = lambda x, mu,sigma: scipy.stats.norm(mu,sigma).cdf(x)
+        mu,sigma = scipy.optimize.curve_fit(f, x, y)[0]
     
-    f = lambda x, mu,sigma: scipy.stats.norm(mu,sigma).cdf(x)
-    mu,sigma = scipy.optimize.curve_fit(f, x, y)[0]
-
+    except RuntimeError:
+        return "Error - curve_fit failed"
+    
     total = 0
     for i in x:
         error = (y[i] - scipy.stats.norm(mu,sigma).cdf(i))**2
         total += error
     
     mse = total / n
-    
-    return scipy.stats.norm(mu,sigma).cdf(x), mse
+
+    return (mu, sigma), mse
 
     pass
 
